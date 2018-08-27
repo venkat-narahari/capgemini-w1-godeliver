1209b3281437305e0f82920d12b34fded37dbd8e
Shivamrajawat <rajawatshivam007@gmail.com>
2018-08-27 11:16:57 +0530

    frontend

--- user-login/src/main/java/com/stackroute/userlogin/repository/UserRepository.java
+++ user-login/src/main/java/com/stackroute/userlogin/repository/UserRepository.java
@@ -9,5 +9,5 @@ import com.stackroute.userlogin.domain.User;
 public interface UserRepository extends CrudRepository<User, Long> {
 //	 User save(User user);
 
-	User findByEmail(String email);
+	public User findByEmail(String email);
 }
