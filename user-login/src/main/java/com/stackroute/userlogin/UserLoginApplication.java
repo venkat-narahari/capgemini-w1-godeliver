4d288852e4c85848deadd78fc6acf3ad62cfe6e9
tejakathari <tejakathari007@gmail.com>
2018-08-28 09:30:17 +0530

    edited user login

--- user-login/src/main/java/com/stackroute/userlogin/UserLoginApplication.java
+++ user-login/src/main/java/com/stackroute/userlogin/UserLoginApplication.java
@@ -2,11 +2,12 @@ package com.stackroute.userlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import com.stackroute.userlogin.config.JwtFilter;
-
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class UserLoginApplication {
 
