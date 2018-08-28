1188f4e73e713ea179dbf11d3552f1e1c53d489f
tejakathari <tejakathari007@gmail.com>
2018-08-28 17:46:17 +0530

    updated kafka

--- user-login/src/main/java/com/stackroute/userlogin/services/UserServiceImpl.java
+++ user-login/src/main/java/com/stackroute/userlogin/services/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.stackroute.userlogin.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.stackroute.userlogin.domain.User;
 import com.stackroute.userlogin.repository.UserRepository;
+import com.stackroute.userprofile.domain.UserProfile;
+
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -12,11 +15,19 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userDao;
 
-	public User save(User user) {
+	@KafkaListener(topics = "userprofile", groupId = "group_json")
+	public User save(UserProfile userProfile) {
+		User user = new User();
+		user.setUserEmail(userProfile.getUserEmail());
+		System.out.println(user.getUserEmail());
+		user.setUserPassword(userProfile.getUserPassword());
+		System.out.println(user.getUserPassword());
 		return userDao.save(user);
 	}
 
 	public User findByEmail(String userEmail) {
 		return userDao.findByUserEmail(userEmail);
 	}
+	
+	
 }
--- user-login/src/main/java/com/stackroute/userprofile/domain/UserProfile.java
+++ user-login/src/main/java/com/stackroute/userprofile/domain/UserProfile.java
@@ -1,8 +1,7 @@
 package com.stackroute.userprofile.domain;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+
 public class UserProfile {
 	private String userName;
 	private String userEmail;
