292a1e0e39beaa6a37322d8c7366a24bb9bb5a8c
tejakathari <tejakathari007@gmail.com>
2018-08-29 21:55:53 +0530

    changes

--- user-login/src/test/java/com/stackroute/userlogin/repository/UserRepositoryTest.java
+++ user-login/src/test/java/com/stackroute/userlogin/repository/UserRepositoryTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.stackroute.userlogin.domain.User;
 
