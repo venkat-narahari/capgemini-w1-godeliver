4240f7554a1ff4d8d76282175a22ce14f3007946
koppela sumanth <sumanth690@gmail.com>
2018-09-12 10:27:43 +0530

    changes in kafka config

--- user-profile/src/main/java/com/stackroute/userprofile/config/KafkaConfiguration.java
+++ user-profile/src/main/java/com/stackroute/userprofile/config/KafkaConfiguration.java
@@ -21,7 +21,7 @@ public class KafkaConfiguration {
 	public ProducerFactory<String, UserProfile> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
 
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.49:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
