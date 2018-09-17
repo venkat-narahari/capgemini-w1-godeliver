ea43e23a1d61c3cb703e0fc5e05842da4ac41487
Shirisha <shiri2181@gmail.com>
2018-09-17 16:48:21 +0530

    changes in kafka

--- recommendation/src/main/java/com/stackroute/recommendation/config/RecommendationConfig.java
+++ recommendation/src/main/java/com/stackroute/recommendation/config/RecommendationConfig.java
@@ -24,7 +24,7 @@ public class RecommendationConfig {
 	public ConsumerFactory<String, Book> consumerFactory() {
 		Map<String, Object> props = new HashMap<>();
 		
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.49:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.61:9092");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "books");
 		//props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.stackroute.bookservice");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -43,9 +43,9 @@ public class RecommendationConfig {
 	public ConsumerFactory<String, UserProfile> userConsumerFactory() {
 		Map<String, Object> config = new HashMap<>();
 
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.49:9092");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.61:9092");
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
-		config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.stackroute.userservice");
+		//config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.stackroute.userservice");
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
