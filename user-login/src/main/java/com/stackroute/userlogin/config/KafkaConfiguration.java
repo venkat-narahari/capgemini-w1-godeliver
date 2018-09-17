aace85b7198390b855e90c5a1e0efc0c7924c9ca
koppela sumanth <sumanth690@gmail.com>
2018-09-17 12:36:16 +0530

    removed hardcoded ip address of kafka in userlogin

--- user-login/src/main/java/com/stackroute/userlogin/config/KafkaConfiguration.java
+++ user-login/src/main/java/com/stackroute/userlogin/config/KafkaConfiguration.java
@@ -2,6 +2,7 @@ package com.stackroute.userlogin.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,12 +18,16 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConfiguration {
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
 	/* kafka consumer factory*/
 	@Bean
 	public ConsumerFactory<String, UserProfile> consumerFactory() {
 		Map<String, Object> config = new HashMap<>();
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.49:9092");
+		
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
