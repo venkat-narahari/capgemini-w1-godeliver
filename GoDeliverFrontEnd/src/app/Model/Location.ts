964ac2bca4e0cf4d4f001c739fa6616b3101ede5
Shivamrajawat <rajawatshivam007@gmail.com>
2018-09-17 10:50:06 +0530

    created model in angular

---
+++ GoDeliverFrontEnd/src/app/Model/Location.ts
@@ -0,0 +1,6 @@
+
+export class Location {
+      public orderLatitude:string;
+      public orderLongitude: string;
+  }
+  
---
+++ GoDeliverFrontEnd/src/app/Model/Slots.ts
@@ -0,0 +1,6 @@
+
+export class Slots {
+      public slotAvailability:boolean[];
+      public slotCost: string[];
+  }
+  
