b53a8b3e240a26dcaabbe527f9d26d76b7f78d32
venkat-narahari <venkat.narahari96@gmail.com>
2018-09-20 14:59:28 +0530

    Frontend: cards component modified

---
+++ GoDeliverFrontEnd/src/app/snackbar/snackbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+
+@Component({
+  selector: 'app-snackbar',
+  templateUrl: './snackbar.component.html',
+  styleUrls: ['./snackbar.component.css']
+})
+export class SnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
