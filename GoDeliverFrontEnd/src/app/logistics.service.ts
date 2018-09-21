13a99e1510b6776bbcb6543f6dc1935be1fa362c
Shivamrajawat <rajawatshivam007@gmail.com>
2018-09-21 11:27:48 +0530

    update timeslots

--- GoDeliverFrontEnd/src/app/logistics.service.ts
+++ GoDeliverFrontEnd/src/app/logistics.service.ts
@@ -1,7 +1,7 @@
 import { Order } from './order-details';
 import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -11,7 +11,7 @@ const httpOptions = {
 })
 export class LogisticService {
 
- private slot_url="http://172.23.239.100:8080/logistics/api/v1/slots";
+ private slot_url="http://172.23.239.124:8086/api/v1/slots";
  private vehicles_url="http://172.23.239.100:8080/logistics/api/v1/"
   constructor(private http: HttpClient) { }
   slots : object[];
@@ -25,9 +25,7 @@ export class LogisticService {
             .set("orderLongitude",order.orderLongitude)
             .set("orderVolume",order.orderVolume)
             .set("orderDate",order.orderDate);
-    return this.http.get<Order[]>(this.slot_url, {params: params}).subscribe(data =>
-      {console.log(data)}
-    );
+    return this.http.get<Order[]>(this.slot_url, {params: params}).pipe(map(body=>body["newOrder"])).pipe(map(body=>body["availableSlots"]));
   }
 }
 
