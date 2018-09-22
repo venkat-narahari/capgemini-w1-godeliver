c16a277304ebc15866b19300ae20906d5e013c39
HarithaBandari <harithabandari154@gmail.com>
2018-09-22 11:07:35 +0530

    cvrp pom changed

---
+++ GoDeliverFrontEnd/src/app/services/authenticationresolver.service.ts
@@ -0,0 +1,9 @@
+// import { Injectable } from '@angular/core';
+// import {Resolve} from '@angular/router';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthenticationresolverService implements Resolve<>{
+
+//   constructor() { }
+// }
