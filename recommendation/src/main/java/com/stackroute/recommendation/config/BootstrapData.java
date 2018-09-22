a679d9b41f374db93fac0af8052573b988930a02
HarithaBandari <harithabandari154@gmail.com>
2018-09-22 10:44:15 +0530

    cvrp pom changed

--- recommendation/src/main/java/com/stackroute/recommendation/config/BootstrapData.java
+++ recommendation/src/main/java/com/stackroute/recommendation/config/BootstrapData.java
@@ -59,19 +59,19 @@ public class BootstrapData implements ApplicationListener<ContextRefreshedEvent>
 		book1.setLanguage("English");
 		bookRepository.save(book1);
 		
-//		Genre genre1 = new Genre(book1.getGenre());
-//		genreRepository.save(genre1);
-//		
-//		OfType ofType1 = new OfType(book1, genre1);
-//		ofTypeRepository.save(ofType1);
-//		
-//		Author author1 = new Author(book1.getAuthor());
-//		authorRepository.save(author1);
-//		
-//		WrittenBy writtenBy1 = new WrittenBy(book1, author1);
-//		writtenByRepository.save(writtenBy1);
-//		
-//		
+		Genre genre1 = new Genre(book1.getGenre());
+		genreRepository.save(genre1);
+		
+		OfType ofType1 = new OfType(book1, genre1);
+		ofTypeRepository.save(ofType1);
+		
+		Author author1 = new Author(book1.getAuthor());
+		authorRepository.save(author1);
+		
+		WrittenBy writtenBy1 = new WrittenBy(book1, author1);
+		writtenByRepository.save(writtenBy1);
+		
+		
 
 		BookListener book2 = new BookListener();
 		book2.setBookISBN_10("564438");
