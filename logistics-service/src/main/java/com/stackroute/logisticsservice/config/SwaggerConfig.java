d7e5c67f6b142acb9141de8a36341bb9c890d166
venkat-narahari <venkat.narahari96@gmail.com>
2018-09-24 15:59:23 +0530

    Swagger correction

--- logistics-service/src/main/java/com/stackroute/logisticsservice/config/SwaggerConfig.java
+++ logistics-service/src/main/java/com/stackroute/logisticsservice/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.stackroute.logistics.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.stackroute.logisticsservice.controller"))
 				.paths(regex("/api/v1.*")).build();
 
 	}
